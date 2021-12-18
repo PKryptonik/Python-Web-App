c4017f06baea709edad7bbdc4b62b6ea42f0d449
PKryptonik <greatgray4790@gmail.com>
2021-12-18 13:07:34 -0700

    inclusion of login/signup fields
    not fully functional just visual currently

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login')
 def login():
-    return "<p>Login<p>"
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout<p>"
+    return "logout"
 
-@auth.route('/sign_up')
+@auth.route('/signup')
 def signup():
-    return '<p>Signup<p>'
+    return render_template("signup.html")
--- website/views.py
+++ website/views.py
@@ -1,16 +1,8 @@
-from flask import (
-    Blueprint,
-    url_for
-)
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
 
-
-@views.route('/sign_up')
-def signup():
-    signup_url = url_for('auth.signup')
-    return 'Signup here: {}'.format(signup_url)
