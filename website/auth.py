e122b58c5bd398cb7d83541132a3ce451e4b8846
PKryptonik <greatgray4790@gmail.com>
2021-12-22 14:24:50 -0700

    Finished implementation of "get_category_class" into templates
    Finished implementation of functions into templates. after a "successful" signup automatically pulls up login page

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, render_template, request, flash
 import time
-
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -15,6 +14,7 @@ def logout():
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
+    
     if request.method == 'POST':
         username = request.form.get('username')
         password1 = request.form.get('password-1')
@@ -23,12 +23,11 @@ def signup():
         if  len(username) < 2:
             flash('Username must be at least 2 characters long.', category='error')
         elif len(password1) < 4:
-            flash('password must be at least 4 characters long.', category='error')
+            flash("password must be at least 4 characters long.", category='error')
         elif password1 != password2:
             flash('Passwords must match in order to proceed.', category='error')
         else:
-            flash('You are all signed up!', category='success')
-            #time.sleep(3)
-            #@auth.route('/login') 
-         
+            flash(message="You are all signed up, please log in.", category='success')
+            time.sleep(3)
+            return login()
     return render_template("signup.html")
--- website/template_helpers.py
+++ website/template_helpers.py
@@ -1,11 +1,16 @@
 
-
-def get_category_class(category):
-    category_map = {'': 'alert-info', 'error': 'alert-danger'}
-    return category_map[category]
+#not needed anymore i think
+#def get_category_class(category):
+#    category_map = {'': 'alert-info', 'error': 'alert-danger'}
+#    return category_map[category]
 
 
 def configure_helpers(app):
     @app.context_processor
-    #def inject_user():
-     #   return dict(category=get_category_class)
+    def utility_processor():
+        def get_category_class(category):
+            category_map = {'': 'alert-info', 'error': 'alert-danger', 'success': 'alert-success'}
+            return category_map[category]
+        return dict(get_category_class=get_category_class)
+     
+     
