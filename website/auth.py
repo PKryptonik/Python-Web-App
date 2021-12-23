57356d0aa431db8a3313c9bb4429ec017db76c5d
PKryptonik <greatgray4790@gmail.com>
2021-12-23 15:42:40 -0700

    removal of commented code
    1 error message at a time bug is not fixed yet

--- website/auth.py
+++ website/auth.py
@@ -27,19 +27,26 @@ def signup():
         password2 = request.form.get('password-2')
 
         if not username:
-            validation_errors['username'] = 'A username is provided'
+            validation_errors['username'] = 'A username is required'
+        else:
+            if len(username) <4:
+                validation_errors['username'] = 'Username must be 4 characters or longer'
 
         if not password1:
             validation_errors['password'] = 'A password is required'
-        else:
-            if password1 != password2:
-                validation_errors['password'] = 'Provided passwords do not match'
-            if len(password1) < 4:
-                validation_errors['password'] = 'Password must be at least 4 characters'
+
+        if password1 != password2:
+            validation_errors['password'] = 'Provided passwords do not match'
+        if len(password1) < 4:
+            validation_errors['password'] = 'Password must be at least 4 characters'
 
         if not len(validation_errors):
             flash('There were no errors', category='success')
+            return redirect(url_for('auth.login'))
         else:
             for message in validation_errors.values():
                 flash(message, category='error')
+                time.sleep(3)
+                return redirect(request.path) #redirect back to the same page
+
     return render_template("signup.html")
