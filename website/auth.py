3d9fff257d8644a695214c9e723966e2c220b9ad
PKryptonik <greatgray4790@gmail.com>
2021-12-29 11:39:40 -0700

    "message" error within template helpers

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, request, flash
-from .models import db
+from flask_login import login_user, login_required, logout_user, current_user
 from flask.helpers import url_for
 from werkzeug.utils import redirect
 from werkzeug.security import generate_password_hash, check_password_hash
+from .models import db
 from .models import User
 import time
 
@@ -26,6 +27,7 @@ def login():
             if user:
                 if check_password_hash(user.password, password):
                     flash('successfully logged in', category='success')
+                    login_user(user, remember=True)
                 return redirect(url_for('views.home'))
         else:
             for message in validation_errors.values():
@@ -35,8 +37,12 @@ def login():
     return render_template("login.html")
 
 @auth.route('/logout')
+@login_required #makes it so you cant access logout unless logged in. flask magic
 def logout():
-    return "logout"
+    logout_user()
+    flash('logged out', category='success')
+    return redirect(url_for('auth.login'))
+
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -49,6 +55,9 @@ def signup():
         password1 = request.form.get('password-1')
         password2 = request.form.get('password-2')
         
+        user = User.query.filter_by(username=username).first()
+        if user:
+            validation_errors['username_error'] = 'This Username is already taken'
         if not username:
             validation_errors['username_error'] = 'A username is required'    
         else:
--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required #makes it so you cant access logout unless logged in. flask magic
 def home():
     return render_template("home.html")
 
