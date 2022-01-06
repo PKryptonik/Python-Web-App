364fcb3e68749ce0c4522b0d9792e2dd2b2f0fc7
PKryptonik <greatgray4790@gmail.com>
2022-01-06 11:16:25 -0700

    deletion confirmation system added

--- website/static/index.js
+++ website/static/index.js
@@ -1,8 +1,8 @@
 function deleteNote(noteId) {
-    fetch("delete-note", {
-        method: "POST",
-        body: JSON.stringify({ noteId: noteId }),
-    }).then((_res) => {
-        window.location.href = "/";
-    });
-}
+    if (window.confirm("Proceeding will perminantly delete all content within the note and cannot be recovered. Proceed?")) {
+        fetch("delete-note", {
+            method: "POST",
+            body: JSON.stringify({ noteId: noteId }),
+        }).then((_res) => {
+            window.location.href = "/";
+})}}
